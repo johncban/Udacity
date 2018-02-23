94a5b47e06b69213aa20d7a85c8d2815cc5a6cd5
Udacity <johncban@hotmail.com>
2018-02-23 17:59:52 -0500

    Update Content
    Update all File Contents (stable).

--- Fullstack/ProjectFour/item_catalog/vagrant/catalog/data_demo.py
+++ Fullstack/ProjectFour/item_catalog/vagrant/catalog/data_demo.py
@@ -21,23 +21,53 @@ session = DBSession()
 
 # Create dummy user
 User1 = User(name="Admin", email="johncban@gmail.com",
-             picture='https://avatars2.githubusercontent.com/u/1221375?s=400&u=c54411d77467978a2268f48ffb44664420594c4e&v=4')
+             picture='https://avatars2.githubusercontent.com\
+             /u/1221375?s=400&u=c54411d77467978a2268f48ffb44664420594c4e&v=4')
 session.add(User1)
 session.commit()
-print "Demo user Successfully Added!"
+print ("Demo user Successfully Added!")
 
 # Component Demo Record
 pccomponents1 = Components(user_id=User1.id, c_name="CPU Processor")
 session.add(pccomponents1)
 session.commit()
-print "PC Component(s) Successfully Added!"
+print("PC Component One Successfully Added!")
 
-
-# PC Part Item Demo Record
-partItem1 = PartItem(user_id=User1.id, p_name="Intel Core i7-7700K Kaby Lake Quad Core",
+partItem1 = PartItem(user_id=User1.id, p_name="Intel\
+                     Core i7-7700K Kaby Lake Quad Core",
                      description="Supports Windows 10 OS, \
                      VR and it can reach 4.5 GHz turbo frequency.",
                      cost="$350", qty="2", pccomponents=pccomponents1)
 session.add(partItem1)
 session.commit()
-print "PC Part Successfully Added!"
+
+partItem2 = PartItem(user_id=User1.id, p_name="AMD RYZEN Threadripper 1900X",
+                     description="It have 8 cores with 16 threads for up \
+                     to 4.0 GHz clock speed and supports DDR4 memory",
+                     cost="$550", qty="3", pccomponents=pccomponents1)
+
+session.add(partItem2)
+session.commit()
+print("PC Part Successfully Added!")
+
+pccomponents2 = Components(user_id=User1.id, c_name="RAM Memory")
+session.add(pccomponents2)
+session.commit()
+print("PC Component Two Successfully Added!")
+
+partItem1 = PartItem(user_id=User1.id, p_name="Corsair Dominator Platinum",
+                     description="DDR3 2400 (PC3 19200) 8GB with CAS \
+                     Latency 11 and 1.65V consumption.",
+                     cost="$180", qty="2", pccomponents=pccomponents2)
+
+session.add(partItem1)
+session.commit()
+
+partItem2 = PartItem(user_id=User1.id, p_name="Corsair Vengeance LPX",
+                     description="DDR4 2666 (PC4 21200) 16GB with CAS \
+                     Latency 16 and 1.2V consumption.",
+                     cost="$180", qty="2", pccomponents=pccomponents2)
+
+session.add(partItem1)
+session.commit()
+print("PC Part Successfully Added!")
